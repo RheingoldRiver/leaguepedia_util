6500ad076679b96f87e8291edf7fd8f1073a6025
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2019-05-13 10:43:08 -0500

    don't care section names

--- roster_toggle_new.py
+++ roster_toggle_new.py
@@ -7,11 +7,10 @@ summary = 'Replace TeamRoster/Button with new' # Set summary
 limit = -1
 startat_page = None
 print(startat_page)
-#startat_page = 'DreamHack Valencia 2015'
+startat_page = 'ESL Benelux 5on5 Opening Cup'
 this_template = site.pages['Template:TeamRoster/Button'] # Set template
 pages = this_template.embeddedin()
 
-SECTION_NAMES = ['Participants', 'Rosters', 'Team Rosters', 'Notable Participants']
 MAX_TEAMS = {
 	'400' : '2',
 	'600' : '3',
@@ -52,32 +51,40 @@ for page in pages:
 	text = page.text()
 	wikitext = mwparserfromhell.parse(text)
 	for section in wikitext.get_sections(flat = True):
-		if section.filter_headings() and section.filter_headings()[0].title.strip() in SECTION_NAMES:
-			section_text = section.strip()
-			lines = section_text.split('\n')
-			if len(lines) > 1 and '{{TeamRoster/Button}}' in lines[1]:
-				if lines[-1].endswith('</div>'):
-					lines[-1] = lines[-1].replace('</div>', '')
-					lines.append('{{RostersEnd}}')
-				elif section_text.count('</div>') == 1:
-					for i, line in enumerate(lines):
-						if '</div>' in line:
-							lines[i] = line.replace('</div>', '{{RostersEnd}}')
-				else:
-					continue
-				lines.pop(1)
-				width = get_width(lines[1])
-				print(width + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-				if width in MAX_TEAMS:
-					lines[1] = get_new_line(page.name, MAX_TEAMS[width])
-					wikitext.replace(section, '\n'.join(lines) + '\n')
-				else:
-					missing_widths.append(width)
-				
-			elif len(lines) > 2 and '{{TeamRoster/Button}}' in lines[1] and '|start}}' in lines[2]:
-				lines[1] = '{{RostersStart}}'
+		section_text = section.strip()
+		has_rosters = False
+		section_wikitext = mwparserfromhell.parse(section_text)
+		for template in section_wikitext.filter_templates():
+			if template.name.matches('TeamRoster'):
+				has_rosters = True
+		if not has_rosters:
+			continue
+		lines = section_text.split('\n')
+		if len(lines) > 2 and '{{TeamRoster/Button}}' in lines[1]:
+			if 'match-by-match' in lines[2]:
+				lines.pop(2)
+			if lines[-1].endswith('</div>'):
+				lines[-1] = lines[-1].replace('</div>', '')
 				lines.append('{{RostersEnd}}')
+			elif section_text.count('</div>') == 1:
+				for i, line in enumerate(lines):
+					if '</div>' in line:
+						lines[i] = line.replace('</div>', '{{RostersEnd}}')
+			else:
+				continue
+			lines.pop(1)
+			width = get_width(lines[1])
+			print(width + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+			if width in MAX_TEAMS:
+				lines[1] = get_new_line(page.name, MAX_TEAMS[width])
 				wikitext.replace(section, '\n'.join(lines) + '\n')
+			else:
+				missing_widths.append(width)
+			
+		elif len(lines) > 2 and '{{TeamRoster/Button}}' in lines[1] and '|start}}' in lines[2]:
+			lines[1] = '{{RostersStart}}'
+			lines.append('{{RostersEnd}}')
+			wikitext.replace(section, '\n'.join(lines) + '\n')
 	newtext = str(wikitext)
 	if text != newtext:
 		print('Saving page %s...' % page.name)
