3d4cbc6e02f54f0725649cd8e5534ae9602c3602
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2019-06-06 05:13:01 -0500

    creating new years for content pages

--- content_data_pages.py
+++ content_data_pages.py
@@ -1,7 +1,8 @@
 from log_into_wiki import *
 from datetime import date, timedelta
+import mwparserfromhell
 
-site = login('bot', 'lol')  # Set wiki
+site = login('me', 'lol')  # Set wiki
 summary = 'Initializing Content Pages'  # Set summary
 
 def allsundays(year):
@@ -11,8 +12,10 @@ def allsundays(year):
 		yield d
 		d += timedelta(days = 7)
 
-for y in [2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019]:
+for y in [2020]:
+	list_of_sundays = ['|' + str(y) + '=']
 	for d in allsundays(y):
+		list_of_sundays.append('* [[Data:ExternalContent/{}|{}]]'.format(d.strftime('%Y-%m-%d'), str(d.strftime('%b %d'))))
 		p = site.pages['Data:ExternalContent/' + str(d)]
 		if p.text() != '':
 			continue
@@ -27,3 +30,15 @@ for y in [2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019]:
 		lines.append('{{ExternalContent/End}}')
 		print('\n'.join(lines))
 		p.save('\n'.join(lines), summary=summary)
+	list_of_sundays.append('}}\n{{Endflatlist}}')
+	template_page = site.pages['Template:External Content Navbox']
+	wikitext = mwparserfromhell.parse(template_page.text())
+	for template in wikitext.filter_templates():
+		if template.name.matches('Navbox'):
+			text = str(template.get('list1').value)
+			text = text.replace('{{Endflatlist}}',
+								'* [[Data:ExternalContent/{}|{}]]\n{{{{Endflatlist}}}}'.format(str(y), str(y)))
+			template.add('list1', text)
+			list_text = template.get('list2').value
+			list_text = list_text.replace('}}\n{{Endflatlist}}', '\n'.join(list_of_sundays))
+	template_page.save(str(wikitext))
