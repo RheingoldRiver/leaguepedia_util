0d74a26a30f33afa5d2bbc3f7e9a4b9f6870547d
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2019-08-12 13:53:32 -0500

    add missing groupby

--- year_stats_by_tournament.py
+++ year_stats_by_tournament.py
@@ -1,14 +1,14 @@
 from log_into_wiki import *
 
 year = '2019'
-tournament = 'LDL 2019 Summer'
+tournament = 'LPL 2019 Summer'
 #########################################
 # modify only above this line
 #########################################
 TEXT = """{{PlayerTabsHeader}}
 {{PlayerYearStats}}"""
 
-SUMMARY = "Autopopulating year player states from tournament %s" % tournament
+SUMMARY = "Autopopulating year player stats from tournament %s" % tournament
 
 site = login('me', 'lol')  # Set wiki
 summary = 'Bot Edit'  # Set summary
@@ -19,6 +19,7 @@ pagelist = site.cargo_pagelist('Link', page_pattern="%s/Statistics/" + year,
 							   tables = 'ScoreboardPlayer',
 							   where = 'OverviewPage="%s"' % overview_page,
 							   fields = 'Link',
+							   group_by = 'Link',
 							   limit = 'max'
 							   )
 for page in pagelist:
