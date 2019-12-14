373aa528daf42e6c8eb22df5c3ac238fef3360a5
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2019-12-13 22:25:06 -0600

    update scratch

--- !!scratch.py
+++ !!scratch.py
@@ -1,18 +1,31 @@
 from log_into_wiki import *
 import mwparserfromhell
 
-site = login('bot', 'lol')  # Set wiki
-summary = 'Add language/date'  # Set summary
+site = login('me', 'lol')  # Set wiki
+summary = 'Bot Edit'  # Set summary
 
 limit = -1
 startat_page = None
 print(startat_page)
 # startat_page = 'asdf'
-this_template = site.pages['Template:PlayerPronunciationFile']  # Set template
+this_template = site.pages['Template:TEMPLATE']  # Set template
 pages = this_template.embeddedin()
 
 # with open('pages.txt', encoding="utf-8") as f:
 # 	pages = f.readlines()
+
+params = {
+	1: 'player',
+	2: 'country',
+	3: 'name',
+	4: 'role',
+	'res': 'residency',
+	'joined': 'joined',
+	'contract': 'contract',
+	'rejoined': 'rejoined',
+}
+
+pages = [ site.pages['Team SoloMid'] ]
 
 passed_startat = False if startat_page else True
 lmt = 0
@@ -28,35 +41,11 @@ for page in pages:
 	text = page.text()
 	wikitext = mwparserfromhell.parse(text)
 	for template in wikitext.filter_templates():
-		if tl_matches(template, ['PlayerPronunciationFile']):
-			recordedby = template.get('recordedby').value.strip()
-			if recordedby == 'Daedalus':
-				template.add('date', '2019-08-14')
-				if 'English' in page.name:
-					template.add('language', 'English')
-				else:
-					template.add('language', 'Danish')
-			elif recordedby == 'Raafaa':
-				template.add('language', 'English')
-				template.add('date', '2019-08-22')
-			elif recordedby == 'Click':
-				template.add('language', 'Russian')
-				template.add('date', '2019-11-21')
-			elif recordedby == 'theaspectoftwilight':
-				template.add('language', 'Spanish')
-				template.add('date', '2019-10-03')
-			elif recordedby == 'katsudion':
-				template.add('language', 'Japanese')
-				template.add('date', '2019-11-21')
-			elif recordedby == 'Grindelwald':
-				template.add('language', 'Vietnamese')
-				template.add('date', '2019-08-07')
-			elif recordedby == 'Elmyra':
-				template.add('date', '2019-10-05')
-	
-	newtext = str(wikitext)
-	if text != newtext:
-		print('Saving page %s...' % page.name)
-		page.save(newtext, summary=summary)
-	else:
-		print('Skipping page %s...' % page.name)
+		if tl_matches(template, ['Listplayer/Current']):
+			template.name = 'TeamMember'
+			for key in params.keys():
+				if template.has(key):
+					val = template.get(key).value.strip()
+					template.remove(key)
+					template.add(params[key], val)
+			print(str(template))
