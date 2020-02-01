722bff45242292639fbbd47db835251d4be961ef
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-01 16:00:44 -0600

    move namespace patrol to per minute

--- cron_per_fifteen.py
+++ cron_per_fifteen.py
@@ -1,7 +1,6 @@
 from cron_tasks import CronTasks
 import match_schedule_hash as ms_hash
 import predictions_verify as predictions
-import patrol_namespaces as patrol
 
 if __name__ == '__main__':
 	tasks = CronTasks(
@@ -10,4 +9,3 @@ if __name__ == '__main__':
 	)
 	tasks.run_revs(ms_hash.run, ['lol'])
 	tasks.run_revs(predictions.run, ['lol'])
-	tasks.run_revs(patrol.run, ['lol', 'cod-esports'])
--- cron_per_minute.py
+++ cron_per_minute.py
@@ -1,6 +1,7 @@
 from cron_tasks import CronTasks
 import ro_news_webhook as ro_news
 import move_blank_edit as move
+import patrol_namespaces as patrol
 
 if __name__ == '__main__':
 	tasks = CronTasks(
@@ -9,3 +10,4 @@ if __name__ == '__main__':
 	)
 	tasks.run_logs(ro_news.run, ['lol'])
 	tasks.run_logs(move.run, ['lol'])
+	tasks.run_revs(patrol.run, ['lol', 'cod-esports'])
