4eb039232f1375081c62b34b7bc2400ed8d8a67e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-23 23:33:48 -0600

    remove old and temp scratch folder

--- old_and_temp_scratch/upload.py
+++
@@ -1,4 +0,0 @@
-from river_mwclient.esports_site import EsportsSite
-site = EsportsSite('lol', user_file='me')
-
-site.upload(open('RuneSprite.png', 'rb'), 'TeamSprite.png', 'Team Sprite', ignore=True)
--- old_and_temp_scratch/wikia.py
+++
@@ -1,6 +0,0 @@
-from river_mwclient.esports_site import EsportsSite
-
-site = log_into_fandom('me', 'leagueoflegends')
-
-print(site.client.pages['Sona'].text())
-
