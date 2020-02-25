77c3a25bf3a5b1e761c23e5e29b6112a31a52b84
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-24 23:59:53 -0600

    add na contract data

--- !!scratch.py
+++ !!scratch.py
@@ -1,21 +1,50 @@
 from river_mwclient.esports_site import EsportsSite
 import mwparserfromhell
-summary = 'auto updating residency to PCS'
-with open('pages.txt', encoding='utf-8') as f:
-	titles = f.readlines()
 
-titles = [_.strip() for _ in titles]
+site = EsportsSite('lol', user_file="me")  # Set wiki
+summary = 'Add source'  # Set summary
 
-site = EsportsSite('lol', user_file="me") # Set wiki
+limit = -1
+startat_page = None
+print(startat_page)
+# startat_page = 'asdf'
+this_template = site.client.pages['Template:ContractData']  # Set template
+pages = this_template.embeddedin()
 
-result = site.client.api(action="query", list="querypage", qppage="DoubleRedirects")
-for item in result['query']['querypage']['results']:
-	source_page = site.client.pages[item['title']]
-	target_title = item['databaseResult']['c_title']
-	target_namespace_number = int(item['databaseResult']['c_namespace'])
-	target_namespace = site.client.namespaces[int(target_namespace_number)]
-	target_page_name = '{}{}'.format(
-		target_namespace + ':' if target_namespace != '' else '',
-		target_title
-	)
-	source_page.save('#redirect[[%s]]' % target_page_name, summary="bot edit!!!!!!!")
+# with open('pages.txt', encoding="utf-8") as f:
+# 	pages = f.readlines()
+
+gcd_text = '{{{{Source|gcd_after={} }}}}'
+gcd_text_current = None
+
+passed_startat = False if startat_page else True
+lmt = 0
+for page in pages:
+	if lmt == limit:
+		break
+	if startat_page and page.name == startat_page:
+		passed_startat = True
+	if not passed_startat:
+		print("Skipping page %s" % page.name)
+		continue
+	lmt += 1
+	gcd_base = 'Archive:Global_Contract_Database/NA/'
+	gcd_page = None
+	text = page.text()
+	wikitext = mwparserfromhell.parse(text)
+	for template in wikitext.filter_templates():
+		if template.name.matches('NewsData/Date'):
+			y = template.get('y').value.strip()
+			m = template.get('m').value.strip()
+			d = template.get('d').value.strip()
+			gcd_page = gcd_base + y + '-' + m + '-' + d
+			gcd_text_current = gcd_text.format(gcd_page)
+		if template.name.matches(['ContractData']):
+			template.add('source', gcd_text_current)
+	
+	newtext = str(wikitext)
+	if text != newtext:
+		print('Saving page %s...' % page.name)
+		page.save(newtext, summary=summary)
+	else:
+		print('Skipping page %s...' % page.name)
