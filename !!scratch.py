9077b7d9e60e8efdebd62e3af65ea073eb4fa999
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-03-27 09:07:37 -0500

    update base_template to use river_mwc classes

--- !!scratch.py
+++ !!scratch.py
@@ -1,24 +1,24 @@
 from river_mwclient.esports_client import EsportsClient
 from river_mwclient.auth_credentials import AuthCredentials
 from river_mwclient.template_modifier import TemplateModifierBase
+from river_mwclient.page_modifier import PageModifierBase
 
-credentials = AuthCredentials(user_file="me")
+credentials = AuthCredentials(user_file="bot")
 site = EsportsClient('lol', credentials=credentials)  # Set wiki
-summary = 'Clean up bundle/loot exclusive to their own args'  # Set summary
+summary = 'Move skin page names'  # Set summary
 
+files = site.cargo_client.query(fields="_pageName=Page, Name, ImageType", tables="SkinImages", limit="max")
 
-class TemplateModifier(TemplateModifierBase):
-    def update_template(self):
-        if not self.current_template.has('special'):
-            return
-        special = self.current_template.get('special').value.strip()
-        if special == 'Loot Exclusive':
-            self.current_template.add('loot_exclusive', 'Yes')
-            self.current_template.remove('special')
-        if special == 'Bundle Exclusive':
-            self.current_template.add('bundle_exclusive', 'Yes')
-            self.current_template.remove('special')
-
-
-TemplateModifier(site, 'ChromaSet/Line',
-                 summary=summary).run()
+for file in files:
+    print(file['Page'])
+    if 'File:Skin ' in file['Page']:
+        continue
+    if file['ImageType'] == 'Square':
+        continue
+    file_type = file['Page'].split('.')[1]
+    new_name = 'File:Skin {} {}.{}'.format(file['ImageType'], file['Name'], file_type)
+    print(new_name)
+    skin_page = site.client.pages[file['Page']]
+    if 'redirect' in skin_page.text().lower():
+        continue
+    skin_page.move(new_name)
--- base_template_edit.py
+++ base_template_edit.py
@@ -1,41 +1,25 @@
 from river_mwclient.esports_client import EsportsClient
 from river_mwclient.auth_credentials import AuthCredentials
-import mwparserfromhell
+from river_mwclient.template_modifier import TemplateModifierBase
+from river_mwclient.page_modifier import PageModifierBase
 
 credentials = AuthCredentials(user_file="me")
-site = EsportsClient('lol', credentials=credentials) # Set wiki
-summary = 'Bot Edit' # Set summary
+site = EsportsClient('lol', credentials=credentials)  # Set wiki
+summary = 'Clean up bundle/loot exclusive to their own args'  # Set summary
 
-limit = -1
-startat_page = None
-print(startat_page)
-#startat_page = 'asdf'
-pages = site.pages_using('TEMPLATE')
 
-# with open('pages.txt', encoding="utf-8") as f:
-# 	pages = f.readlines()
-
-passed_startat = False if startat_page else True
-lmt = 0
-for page in pages:
-	if lmt == limit:
-		break
-	if startat_page and page.name == startat_page:
-		passed_startat = True
-	if not passed_startat:
-		print("Skipping page %s" % page.name)
-		continue
-	lmt += 1
-	text = page.text()
-	wikitext = mwparserfromhell.parse(text)
-	for template in wikitext.filter_templates():
-		if template.name.matches(['TEMPLATEYOUCAREABOUT']):
-			print(template) # here so it doesn't think there's an error
-			# TODO
+class PageModifier(PageModifierBase):
+	def update_plaintext(self):
+		return
 	
-	newtext = str(wikitext)
-	if text != newtext:
-		print('Saving page %s...' % page.name)
-		page.save(newtext, summary=summary)
-	else:
-		print('Skipping page %s...' % page.name)
+	def update_wikitext(self):
+		return
+
+
+class TemplateModifier(TemplateModifierBase):
+	def update_template(self):
+		return
+
+
+TemplateModifier(site, 'TEMPLATEYOUCAREABOUT',
+				 summary=summary).run()
