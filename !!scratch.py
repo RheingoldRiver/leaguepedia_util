7329e73f6bfdf475cf43f6b493c4e1e576cae783
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-05-23 15:51:51 -0500

    all the template stuff is now in cron per_hour

--- !!scratch.py
+++ !!scratch.py
@@ -4,21 +4,7 @@ from river_mwclient.template_modifier import TemplateModifierBase
 
 credentials = AuthCredentials(user_file="bot")
 site = EsportsClient('lol', credentials=credentials)  # Set wiki
-summary = 'Use just |player=, no |link='  # Set summary
+summary = 'Use page_type instead of checkboxIsPersonality'  # Set summary
 
 
-class TemplateModifier(TemplateModifierBase):
-	def update_template(self):
-		if not self.current_template.has('link'):
-			return
-		player = self.current_template.get('player').value.strip()
-		link = self.current_template.get('link').value.strip()
-		if not link.lower().startswith(player.lower()):
-			return
-		self.current_template.remove('link')
-		self.current_template.add('player', link)
-
-
-TemplateModifier(site, 'TeamRoster/Line',
-                 startat_page="Prime League 2nd Division/2020 Season/Spring Season",
-				 summary=summary).run()
+print(site.client.pages['Data:News/2020-05-17'])
--- weekly_utils_main.py
+++ weekly_utils_main.py
@@ -1,17 +1,12 @@
 # weekly is a lie, this runs twice-daily
 
-import mwparserfromhell, datetime
-import weekly_utils as utils
+import datetime
 import scrape_runes
-from pick_ban_validator import PickBanValidator
 from river_mwclient.esports_client import EsportsClient
 from river_mwclient.auth_credentials import AuthCredentials
 
 credentials = AuthCredentials(user_file="me")
 site = EsportsClient('lol', credentials=credentials) # Set wiki
-pick_ban_validator = PickBanValidator(site)
-
-limit = -1
 
 now_timestamp = datetime.datetime.utcnow().isoformat()
 with open('daily_last_run.txt','r') as f:
@@ -32,52 +27,12 @@ revisions = site.client.api('query', format='json',
 pages = []
 pages_for_runes = []
 
-def report_errors(report_page, page, errors):
-	text = report_page.text()
-	error_text = '\n* '.join([e.args[0] for e in errors])
-	newtext = text + '\n==Python Error Report==\nPage: [[{}]] Messages:\n* {}'.format(page, error_text)
-	report_page.save(newtext)
-
 for revision in revisions['query']['recentchanges']:
 	title = revision['title']
 	if title not in pages:
 		pages.append(title)
 		if title.startswith('Data:'):
 			pages_for_runes.append(title)
-
-lmt = 1
-for page in pages:
-	if lmt == limit:
-		break
-	lmt+=1
-	try:
-		p = site.client.pages[page]
-	except KeyError:
-		# print(page)
-		continue
-	else:
-		text = p.text()
-		wikitext = mwparserfromhell.parse(text)
-		errors = []
-		for template in wikitext.filter_templates():
-			try:
-				if template.name.matches('Infobox Player'):
-					utils.fixInfoboxPlayer(template)
-				elif template.name.matches('Infobox Team'):
-					utils.fixInfoboxTeam(template)
-				elif template.name.matches('PicksAndBansS7') or template.name.matches('PicksAndBans'):
-					utils.fixPB(pick_ban_validator, template)
-				elif template.name.matches('Listplayer/Current/End'):
-					template.add(1, '')
-			except Exception as e:
-				errors.append(e)
-		newtext = str(wikitext)
-		if text != newtext:
-			# print('Saving page %s...' % page)
-			p.save(newtext,summary='Automated error fixing (Python)',tags='daily_errorfix')
-		if len(errors) > 0:
-			report_page = site.client.pages['User talk:RheingoldRiver']
-			report_errors(report_page, page, errors)
 
 success_page = site.client.pages['User:RheingoldRiver/Maint Log']
 text = success_page.text()
