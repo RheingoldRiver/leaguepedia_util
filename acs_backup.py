3ace58d929e8a22c673a9395ce9a1b1caeb371c7
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-09-04 21:12:59 -0500

    better error correction

--- acs_backup.py
+++ acs_backup.py
@@ -24,26 +24,27 @@ def get_metadata(row, realmx, game_idx, game_hashx):
         realmx,
         game_idx,
         game_hashx,
-        row['GameId'],
-        row['MatchId'],
-        row['N GameInMatch'],
+        row['GameId'].replace('&amp;', '&'),
+        row['MatchId'].replace('&amp;', '&'),
+        row['N_GameInMatch'],
         row['OverviewPage']
     )
 
 def main():
 
-    credentials = AuthCredentials(user_file='me')
+    credentials = AuthCredentials(user_file='bot')
     site = EsportsClient('lol', credentials=credentials)
     acs = ACS()
 
     result = site.cargo_client.query(
-        tables='MatchScheduleGame=MSG',
-        fields='MatchHistory, GameId, OverviewPage, MatchId, N_GameInMatch',
-        where='MatchHistory LIKE "%gameHash%"'
+        tables='MatchScheduleGame=MSG,AcsMetadata=ACS',
+        join_on='MSG.GameId=ACS.GameId',
+        fields='MSG.MatchHistory=MatchHistory, MSG.GameId=GameId, MSG.OverviewPage=OverviewPage, MSG.MatchId=MatchId, MSG.N_GameInMatch=N_GameInMatch, MSG._pageName=Page',
+        where='MatchHistory LIKE "%gameHash%" AND ACS.GameId IS NULL'
     )
 
-    passed_startat = False
-    startat = 'ESPORTSTMNT02 1271211'
+    passed_startat = True
+    startat = 'FRA1TMNT1 210419'
 
     for game in result:
         re_match = re.match(r'^.*match-details/(.+?)/(.+?)\?gameHash=(.+?)(?:&a?m?p?;?tab=.*)?$', game['MatchHistory'])
@@ -58,11 +59,19 @@ def main():
         print('Processing {} now, hash is {}...'.format(fingerprint, game_hash))
         try:
             site.save_title('V4 data:{}_{}'.format(realm, game_id), json.dumps(acs.get_game(realm, game_id, game_hash)))
+
         except HTTPError:
             with open('acs_errors.txt', 'a') as f:
                 f.write('\n{} {} {}'.format(realm, game_id, game_hash))
+            print(game['MatchHistory'])
+            print(game['Page'])
             continue
-        site.save_title('V4 data:{}_{}/Timeline'.format(realm, game_id), json.dumps(acs.get_game_timeline(realm, game_id, game_hash)))
+        try:
+            site.save_title('V4 data:{}_{}/Timeline'.format(realm, game_id), json.dumps(acs.get_game_timeline(realm, game_id, game_hash)))
+        except HTTPError:
+            print(game['MatchHistory'])
+            print(game['Page'])
+            pass
         site.save_title('V4 metadata:{}_{}'.format(realm, game_id), get_metadata(game, realm, game_id, game_hash))
 
 if __name__ == '__main__':
