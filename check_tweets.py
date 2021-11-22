391fe043459d1c69f3319b507eaa2812844326f7
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2021-11-22 02:21:42 -0300

    Update check_tweets.py
    Now using mwrogue, not printing to console, and client updated to twitter_client. I still have doubts with how to handle ratelimit

--- check_tweets.py
+++ check_tweets.py
@@ -1,4 +1,5 @@
-import mwclient
+from mwrogue.esports_client import EsportsClient
+from mwrogue.esports_client import AuthCredentials
 import tweepy
 from datetime import date, timedelta
 import time
@@ -12,21 +13,22 @@ bearertoken = os.getenv("bearertoken")
 checkfrom = date.today() - timedelta(days=4)
 
 def main():
-	client = tweepy.Client(bearer_token=bearertoken)
+	twitter_client = tweepy.Client(bearer_token=bearertoken)
+	credentials = AuthCredentials(user_file="me")
+	site = EsportsClient("lol", credentials=credentials)
 
-	site = mwclient.Site('lol.fandom.com', path='/')
-	response = site.api('cargoquery',
+	response = site.cargo_client.query(
 		limit = "max",
 		tables = "NewsItems=NI",
-		fields = "NI.Source",
+		fields = "NI.Source, NI._pageName=pageName, NI.N_LineInDate",
 		where = 'NI.Source IS NOT NULL AND NI.Date_Sort >= "{}"'.format(checkfrom),
 		order_by = "NI.Date_Sort DESC"
 	)
 
-	print("I have the data from the wiki!")
-
-	for source in response["cargoquery"]:
-		sourcelist = source["title"]["Source"].split(":::")
+	for source in response:
+		sourcelist = source["Source"].split(":::")
+		datapage = source["pageName"]
+		lineindate = source["N LineInDate"]
 		for source in sourcelist:
 			if not source:
 				continue
@@ -34,21 +36,20 @@ def main():
 			if source[2] == "twitter.com":
 				splitStatus = re.search(r"status/([0-9]+)", source[0])[1]
 				if not splitStatus:
-					print("Error with tweet {}".format(str(source[0])))
+					site.log_error_content("\nCan't get tweet id for: {1}".format(source[0]))
 				try:
-					r = client.get_tweet(splitStatus)
+					r = twitter_client.get_tweet(splitStatus)
 				except tweepy.TooManyRequests:
-					print("Ratelimited! Waiting 30 seconds")
 					time.sleep(30)
-					r = client.get_tweet(splitStatus)
+					r = twitter_client.get_tweet(splitStatus)
 				if not r.errors:
 					continue
 				elif r.errors[0]["title"] == "Not Found Error":
-					print("Not found! {}".format(str(source[0])))
+					site.log_error_content("\nTweet link: {0} Data page: {1} - Line {2}".format(str(source[0]), str(datapage), str(lineindate)))
 				else:
-					print("Error with tweet! {0} - {1}".format(str(r.errors[0]["title"]), str(source[0])))
+					site.log_error_content("\nFailure when trying to get {0}, Error Title: {1}".format(str(source[0]), str(r.errors[0]["title"])))
 
-	print("Done!")
+	site.report_all_errors("Deleted Tweets")
 
 if __name__ == '__main__':
     main()
